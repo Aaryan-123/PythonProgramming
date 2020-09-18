86570fe7ea1c6d4453d44bd026e969f8490dc8d6
Aaryan <70795502+Aaryan-123@users.noreply.github.com>
2020-09-18 21:56:32 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+print('Hello, World!')
+print(1 + 2)
+print(5 * 5)
+print()
+print("The end", "or is it?", 'learn python programming',3)
---
+++ sliceback.py
@@ -0,0 +1,10 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[::-1]
+print(backwards)
+backwards = letters[16:13:-1]
+print(backwards)
+backwards = letters[4::-1]
+print(backwards)
+backwards = letters[:17:-1]
+print(backwards)
